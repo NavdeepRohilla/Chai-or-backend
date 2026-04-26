70e6a607faf98c93391b5aef2995b49cbeb5ace3
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-04-26 19:16:19 +0530

    feat: Implement JWT authentication with login and logout functionality, add verifyJWT middleware

--- Chai-backend/src/controllers/user.controller.js
+++ Chai-backend/src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from "../models/user.models.js"
 import { uploadToCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try{
+        const user = await user.findOne(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        user.save({validateBeforeSave: false})
+
+        return {accessToken , refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
     // get user details from  frontend
     // validation - not empty
@@ -83,8 +98,90 @@ const registerUser = asyncHandler(async (req, res) => {
        
 })
 
+const loginuser = asyncHandler(async (req, res) => {
+    /* 
+    req body -> data lao
+    username or email base
+    find the user 
+    password check
+    access and refresh token
+    send cookies 
+    response successfully logged in
+    */
 
+    const {email, username, password} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400, "username or email is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials.")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshtoken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                user: loggedUser, accessToken, refreshToken
+            },
+            "user logged in Successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    // req.user._id
+
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httponly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessTokens", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"))
+})
 
 export {
     registerUser,
+    loginuser,
+    logoutUser
 }                                   
--- Chai-backend/src/models/user.models.js
+++ Chai-backend/src/models/user.models.js
@@ -61,7 +61,7 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password , this.password)
 }
 
-userSchema.methods.generateAccessToken = function () {
+userSchema.methods.generateAccessToken = function () { // Short lived token
     jwt.sign(
         {
             _id: this._id,
@@ -75,7 +75,7 @@ userSchema.methods.generateAccessToken = function () {
         }
     )
 }
-userSchema.methods.generateRefreshTokens = function () {
+userSchema.methods.generateRefreshTokens = function () { // Long lived token
     jwt.sign(
         {
             _id: this._id,
